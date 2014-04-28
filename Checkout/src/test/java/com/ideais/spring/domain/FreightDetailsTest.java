10e25ce1017a645d51b41b57d19e124e6bb6dc8c
Mariana Lima <mariana.lima@ideais.com.br>
2014-04-28 02:59:31 -0300

    testes

--- Checkout/src/test/java/com/ideais/spring/domain/FreightDetailsTest.java
+++ Checkout/src/test/java/com/ideais/spring/domain/FreightDetailsTest.java
@@ -1,15 +1,28 @@
 package com.ideais.spring.domain;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FreightDetailsTest {
 
 	FreightDetails freight = new FreightDetails();	
 	
+	@Before
+	public void setUp() {
+		freight.setDeliveryDays("2");
+		freight.setStoreZipCode("26600000");
+		freight.setServiceType("PAX");
+		freight.setDestinationZipCode("26600000");
+	}
+	
 	@Test
 	public void check_if_message_appears_when_it_has_no_delivery_days() {
+		freight.setDeliveryDays(null);
+		
 		assertEquals("Digite o CEP acima para calcular o prazo de entrega.", freight.getDeliveryDays());
 	}
 	
@@ -21,53 +34,49 @@ public class FreightDetailsTest {
 	
 	@Test
 	public void check_format_with_delivery_days_higher_than_1() {
-		freight.setDeliveryDays("2");
 		assertEquals("2 dias úteis.", freight.getDeliveryDays());
 	}
 	
 	@Test
 	public void check_if_freight_was_not_calculated_with_delivery_days_equals_null() {
-		freight.setStoreZipCode("26600000");
-		freight.setServiceType("PAX");
-		freight.setDestinationZipCode("26600000");
+		freight.setDeliveryDays(null);
+		
 		assertFalse(freight.wasCalculated());
 	}
 
 	@Test
 	public void check_if_freight_was_not_calculated_with_store_zip_code_equals_null() {
-		freight.setDeliveryDays("2");
-		freight.setServiceType("PAX");
-		freight.setDestinationZipCode("26600000");
+		freight.setStoreZipCode(null);
+		
 		assertFalse(freight.wasCalculated());
 	}
 
 	@Test
 	public void check_if_freight_was_not_calculated_with_service_type_equals_null() {
-		freight.setDeliveryDays("2");
-		freight.setStoreZipCode("26600000");
-		freight.setDestinationZipCode("26600000");
+		freight.setServiceType(null);
+		
 		assertFalse(freight.wasCalculated());
 	}
 
 	@Test
 	public void check_if_freight_was_not_calculated_with_destination_zip_code_equals_null() {
-		freight.setDeliveryDays("2");
-		freight.setStoreZipCode("26600000");
-		freight.setServiceType("PAX");
+		freight.setDestinationZipCode(null);
+		
 		assertFalse(freight.wasCalculated());
 	}
 	
 	@Test
 	public void check_if_freight_was_not_calculated_with_all_info_equals_null() {
+		freight.setDeliveryDays(null);
+		freight.setStoreZipCode(null);
+		freight.setServiceType(null);
+		freight.setDestinationZipCode(null);
+		
 		assertFalse(freight.wasCalculated());
 	}
 	
 	@Test
 	public void check_if_freight_was_calculated_with_all_info() {
-		freight.setDeliveryDays("2");
-		freight.setStoreZipCode("26600000");
-		freight.setServiceType("PAX");
-		freight.setDestinationZipCode("26600000");
 		assertTrue(freight.wasCalculated());	
 	}
 }
